b4769aacdbab2162699f9bc068e101a74d90d18d
DavidLondo <davidlondonopalacio@gmail.com>
2024-08-13 10:14:33 -0500

    Add front directory with home page

---
+++ FrontLumiere/lumiere-2/src/App.js
@@ -0,0 +1,24 @@
+import { gsap, Power3 } from "gsap";
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Images from "./Images";
+import Features from "./Features";
+
+function App() {
+  let tl = new gsap.timeline();
+  let ease = Power3.easeOut;
+  return (
+    <div className="hero">
+      <Header timeline={tl} ease={ease} />
+      <div className="container">
+        <Content timeline={tl} />
+        <Images timeline={tl} ease={ease} />
+      </div>
+      <Features/> {}
+    </div>
+  );
+}
+
+export default App;
