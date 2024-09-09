ea23277a09ee2be56002c9f21437b08684e56ef5
Tazana2 <danisanmez@gmail.com>
2024-09-09 00:10:52 -0500

    Fix error in profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 
 function Profile() {
     const [user, setUser] = useState({})
+    const [date, setDate] = useState("")
 
     useEffect(() => {
         getUserProfile()
@@ -12,14 +13,17 @@ function Profile() {
         api.get("/users/api/user/profile/")
             .then((res) => {
                 setUser(res.data)
+                setDate(new Date(res.data.created_at).toDateString())
             })
             .catch((err) => console.log(err))
     }
     return (
         <>
             <h1>Profile</h1>
-            <p>Username: { user }</p>
+            <p>Username: { user.username }</p>
             <p>Email: { user.email }</p>
+            <p>Bio: { user.bio }</p>
+            <p>Created at: { date }</p>
         </>
     )
 }
