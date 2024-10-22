1a310dbb7363d8718f23373aff0a124af3ccb4f1
DavidLondo <davidlondonopalacio@gmail.com>
2024-10-22 09:10:38 -0500

    Modify Forum Page

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,17 +1,20 @@
-import React from "react"
-import "../styles/Post.css"
+import React from "react";
+import "../styles/Post.css";
 
-function Post({post, onDelete}) {
-    const formatteDate = new Date(post.date_posted).toLocaleDateString()
+function Post({ post, onDelete }) {
+    const formattedDate = new Date(post.date_posted).toLocaleDateString();
 
-    return <div className="post-container">
-        <p className="post-title">{ post.title }</p>
-        <p className="post-content">{ post.content }</p>
-        <p className="post-date">By {post.author} on { formatteDate }</p>
-        <button className="delete-button" onClick={() => onDelete(post.id)}>
-            Delete
-        </button>
-    </div>
+    return (
+        <div className="post-container">
+            <p className="post-title">{post.title}</p>
+            <hr className="post-divider" /> {/* Línea divisoria */}
+            <p className="post-content">{post.content}</p>
+            <p className="post-date">Por {post.author} el {formattedDate}</p>
+            <button className="delete-button" onClick={() => onDelete(post.id)}>
+                Eliminar
+            </button>
+        </div>
+    );
 }
 
-export default Post
+export default Post;
--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -1,84 +1,96 @@
-import { useState, useEffect } from "react"
-import api from "../api"
-import { Post } from "../components"
-import "../styles/Forum.css"
+import React, { useState, useEffect } from 'react';
+import api from '../api'; 
+import Post from '../components/Post'; // Verifica que la ruta sea correcta
+import "../styles/Forum.css";
 
 function Forum() {
-    const [posts, setPosts] = useState([])
-    const [title, setTitle] = useState("")
-    const [content, setContent] = useState("")	
+    const [posts, setPosts] = useState([]);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
     useEffect(() => {
-        getPosts()
-    }, [])
+        getPosts();
+    }, []);
 
     const getPosts = () => {
         api.get("/posts/api/")
             .then((res) => res.data)
             .then((data) => {
-                setPosts(data)
+                setPosts(data);
             })
-            .catch((err) => console.log(err))
-    }
+            .catch((err) => console.log(err));
+    };
 
     const deletePost = (id) => {
         api.delete(`/posts/api/delete/${id}/`)
             .then((res) => {
                 if (res.status === 204) {
-                    setPosts(posts.filter((post) => post.id !== id))
-                    alert("Post deleted successfully")
-                } else alert("Failed to delete post")
+                    setPosts(posts.filter((post) => post.id !== id));
+                    alert("Post eliminado con éxito");
+                } else alert("Error al eliminar el post");
             })
-            .catch((err) => alert(err))
-    }
+            .catch((err) => alert(err));
+    };
 
     const createPost = (e) => {
-        e.preventDefault()
-        api.post("/posts/api/", {title, content})
-        .then((res) => {
-            if (res.status === 201) {
-                setPosts([...posts, res.data])
-                alert("Post created successfully")
-            } else alert("Failed to create post")
-        })
-        .catch((err) => alert(err))
-    }
-
+        e.preventDefault();
+        api.post("/posts/api/", { title, content })
+            .then((res) => {
+                if (res.status === 201) {
+                    setPosts([...posts, res.data]);
+                    alert("Post created successfully");
+    
+                    // Limpiar los campos de texto
+                    setTitle(""); // Limpiar el campo del título
+                    setContent(""); // Limpiar el campo del contenido
+                } else alert("Failed to create post");
+            })
+            .catch((err) => alert(err));
+    };
+    
     return (
-        <>
-            <div className="body">
-                <h2>Foro</h2>
-                <h3>Crea un Post!</h3>
-                <form onSubmit={createPost}>
-                    <label htmlFor="title">T&iacute;tulo:</label>
-                    <br />
-                    <input
-                        type="text"
-                        name="title" 
-                        id="title" 
-                        required 
-                        onChange={(e) => setTitle(e.target.value)}
-                        value={title}
-                    />
-                    <br />
-                    <label htmlFor="content">Contenido:</label>
-                    <br />
-                    <textarea 
-                        name="content" 
-                        id="content"
-                        required
-                        value={content}
-                        onChange={(e) => setContent(e.target.value)}
-                    ></textarea>
-                    <br />
-                    <input type="submit" value="submit" className="btn" />
-                </form>
-                { posts.map((post) => 
-                    <Post post={post} onDelete={deletePost} key={post.id}/>
-                )}
+        <div className="body-forum">
+            <div className="forum-header-card">
+                <h2 className="forum-header-title">Foro</h2>
+                <h3 className="forum-header-subtitle">Crea un post!</h3>
             </div>
-        </>
-    )
+
+            <div className="forum-content"> 
+                <div className="forum-create-post">
+                    <form onSubmit={createPost}>
+                        <label className="forum-label" htmlFor="title">T&iacute;tulo:</label>
+                        <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            required
+                            className="forum-input"
+                            onChange={(e) => setTitle(e.target.value)}
+                            value={title}
+                        />
+                        <label className="forum-label" htmlFor="content">Contenido:</label>
+                        <textarea
+                            name="content"
+                            id="content"
+                            required
+                            className="forum-textarea"
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                        ></textarea>
+                        <input type="submit" value="Publicar" className="forum-btn" /> 
+                    </form>
+                </div>
+
+                <div className="forum-posts"> 
+                    {posts.map((post) => (
+                        <div className="forum-post-card" key={post.id}>
+                            <Post post={post} onDelete={deletePost} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Forum
+export default Forum;
