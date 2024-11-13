965bb7a91b8c5a0e4027f814ce16d3d0969329a3
Tazana2 <danisanmez@gmail.com>
2024-11-13 10:58:11 -0500

    Delete stop camera view

--- backend/prueba/urls.py
+++ backend/prueba/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('video_feed/', views.video_feed, name='video_feed'),
     path('get_prediction/', views.get_prediction, name='get_prediction'),  # Nueva ruta para predicción
-    path("stop_camera/", views.stop_camera, name="stop_camera"),  # Nueva ruta para detener la cámara
 ]
